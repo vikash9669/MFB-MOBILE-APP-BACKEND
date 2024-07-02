7872d1d1e497b05af490cb7b343adb3e4d955885
Jitesh Kriplani <jiteshkriplani0206@gmail.com>
2024-07-02 14:16:11 +0530

    removed logging

--- util/database.js
+++ util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize").Sequelize;
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER_NAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "mysql",
+    logging: false
 });
 
 module.exports = sequelize;
