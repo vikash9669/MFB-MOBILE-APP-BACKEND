e73c9ab577d6a55dab867c2be732e01eae278a0f
Jitesh Kriplani <jiteshkriplani0206@gmail.com>
2024-10-05 17:55:55 +0530

    added login flag

--- controllers/products.js
+++ controllers/products.js
@@ -172,7 +172,13 @@ const getRestaurantsList = async (req, res) => {
         {
           model: User,
           as: "user",
-          attributes: ["user_id", "user_image", "user_name", "user_active"],
+          attributes: [
+            "user_id",
+            "user_image",
+            "user_name",
+            "user_active",
+            "user_login",
+          ],
         },
       ],
       order: [["business_order", "ASC"]],
