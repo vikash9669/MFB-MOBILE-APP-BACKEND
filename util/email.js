a2e99074363cc0508517cdce90984bf8bd19a752
Jitesh Kriplani <jiteshkriplani0206@gmail.com>
2025-09-13 14:42:00 +0530

    changed mail config

--- util/email.js
+++ util/email.js
@@ -2,12 +2,15 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = {
