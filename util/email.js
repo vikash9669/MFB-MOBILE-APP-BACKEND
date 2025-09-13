c1d8e03bce3eb23d83335c974bd2b6683f842e11
Jitesh Kriplani <jiteshkriplani0206@gmail.com>
2025-09-13 14:55:17 +0530

    stopped email

--- util/email.js
+++ util/email.js
@@ -2,15 +2,12 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: 587,
-  secure: false, // true for 465, false for other ports
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false
-  }
 });
 
 module.exports = {
